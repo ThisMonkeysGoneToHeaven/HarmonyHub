8ae849c2e1e2ba71982b340d4cf332cda373a554
Anant Agarwal <anantagarwal@Anants-MacBook-Air.local>
2024-05-18 13:11:44 +0530

    added an explanation about not explicitly mentioning PORT number in the url for render

--- frontend/src/utils/constants.mjs
+++ frontend/src/utils/constants.mjs
@@ -6,6 +6,10 @@ const serverIp = `harmonyhub-u29y.onrender.com`;
 const protocol = `https`;
 const PORT = 10000;
 
+/*
+No need to enter the port number when app is hosted on a live server on an online service like render, because they route the user to the right port by using the domain name, internally. Since they do it for us, we don't have to do it explicitly.
+*/
+
 const backendServerBaseURL = `${protocol}://${serverIp}${serverIp == `localhost` ? `:3000` : ``}`;
 
 export {backendServerBaseURL};
